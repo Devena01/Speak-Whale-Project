490fc6b2468e3dcda1fde81742c13e0e583d3012
Ana Devenish <132521746+Devena01@users.noreply.github.com>
2023-06-12 22:27:05 -0400

    Create main j.s

---
+++ main j.s
@@ -0,0 +1,24 @@
+let input = 'turpentine and turtles'
+const vowels = ['a','e','i','o', 'u'];
+let resultArray = [];
+//outer loop
+for (let i = 0; i<input.length; i++){
+if (input[i] === 'e'){
+resultArray.push(input[i]);
+}
+
+if (input[i] === 'u'){
+resultArray.push(input[i]);
+}
+
+for (let j= 0; j<vowels.length; j++){
+if(input[i] === vowels[j]){
+
+ resultArray.push(input[i]);
+}
+}
+}
+const resultString = resultArray.join('').toUpperCase();
+console.log(resultString);
+
+
